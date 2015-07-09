628981954951f976f79c0543227b996e8f547d07
Brian Ballsun-Stanton <brian@fedarch.org>
2015-07-09 16:57:59 +1000

    doesn't work

--- ochre.py
+++ ochre.py
@@ -248,7 +248,7 @@ for aenttype in exportCon.execute("select aenttypeid, aenttypename from aenttype
 
 
 		formattedIdents = ET.SubElement(aent, "formattedIdentifier")
-		if "identifier" in formattedIdentifiers["%s" % (row[0])]:
+		if "%s" % (row[0]) in formattedIdentifiers:
 			formattedIdents.text = formattedIdentifiers["%s" % (row[0])]['identifier']
 		else:
 			formattedIdents.text = "ERROR-NoIdentifier!"			
